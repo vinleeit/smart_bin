327adb4e511dbb315110d25d88eda07f26541438
Arvin Lee <xiaoming@192.168.1.7>
2022-01-13 12:36:45 +0700

    Changes: - Differentiating motion and height SRF05 - Update functions and variables name - Update comments and documentaries

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,18 @@ const int clk = 32;
 HX711 scale;
 const float calibrationFactor = -206650; // Adjusted accordingly
 
-// SRF05 pins
-const int trigger = 5;
-const int echo = 18;
-// SRF05
-SRF05 srf(trigger, echo);
+// Motion SRF05 pins
+const int motionTrigger = 5;
+const int motionEcho = 18;
+// Motion SRF05
+SRF05 motionSrf(motionTrigger, motionEcho);
+const int maxMotionDist = 8; // In centimeters
+
+// Height SRF05 pins
+const int heightTrigger = 25;
+const int heightEcho = 26;
+// Height SRF05
+SRF05 heightSrf(heightTrigger, heightEcho);
 
 // Servo pins
 const int servoPin = 13;
@@ -46,7 +53,8 @@ void setupLoadCell()
 
 void setupUltrasonic()
 {
-  srf.setCorrectionFactor(1.035);
+  motionSrf.setCorrectionFactor(1.035);
+  heightSrf.setCorrectionFactor(1.035);
 }
 
 void setupServo()
@@ -54,16 +62,22 @@ void setupServo()
   servo.attach(servoPin);
 }
 
-// Get the distance measured from ultrasonic sensor.
-double getDist()
+// For motion detection to open lid
+bool detectMotion()
 {
-  return srf.getCentimeter();
+  return (motionSrf.getCentimeter() <= 8);
 }
 
-// Display distance into the LCD. The distance is obtained from the [getDist] function.
-void displayDist()
+// Get rubbish height / space left inside the bin
+double getHeight()
 {
-  double dist = getDist();
+  return heightSrf.getCentimeter();
+}
+
+// Display height of rubbish inside the bin. The distance is obtained from the [getHeight] function.
+void displayHeight()
+{
+  double dist = getHeight();
   int roundedDist = (int)(dist + 0.5); // Round distance to decimal point
 
   lcd.setCursor(0, 0);     // First row
@@ -120,8 +134,8 @@ void moveServo()
 // The lid of the rubbish bin will open. Otherwise, it will close.
 void lidController()
 {
-  double dist = getDist();
-  if (dist < 8)
+  const int ok = detectMotion();
+  if (ok)
   {
     if (!isLidOpen)
     {
@@ -161,7 +175,7 @@ void setup()
 // Body
 void loop()
 {
-  // displayDist();
+  displayHeight();
   // displayWeight();
   // moveServo();
   lidController();
